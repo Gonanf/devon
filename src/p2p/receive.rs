2cfc9def4fb507c10789d35a5d932a98d03e2e3d
Gonanf <gabysolotorevsky100@gmail.com>
2025-10-20 15:37:48 -0300

    feat(Moved p2p)

---
+++ src/p2p/receive.rs
@@ -0,0 +1,82 @@
+use anyhow::{Context, Ok, Result};
+use clap::Parser;
+use iroh::{protocol::Router, Endpoint, SecretKey, Watcher};
+use iroh_blobs::{
+    api::{
+        blobs::{self, AddPathOptions, ExportMode, ExportOptions, ImportMode},
+        remote::GetProgressItem,
+        tags::TagInfo, Store,
+    }, format::collection::Collection, get::{self, Stats}, store::{fs::FsStore, mem::MemStore}, ticket::{self, BlobTicket}, BlobFormat, BlobsProtocol, Hash
+};
+use n0_future::{BufferedStreamExt, IterExt, StreamExt};
+use std::{
+    ffi::OsString,
+    fs::{self, create_dir_all},
+    path::{self, Path, PathBuf},
+};
+use tokio::{
+    fs::{create_dir, remove_dir_all},
+    sync::mpsc,
+};
+use walkdir::WalkDir;
+use crate::cli::commands::*;
+
+
+pub async fn receive(path: Option<PathBuf>, ticket: BlobTicket) -> Result<()> {
+    let mut rng = rand::rngs::OsRng;
+    let key =  SecretKey::generate(&mut rng);
+    let endpoint = Endpoint::builder().secret_key(key).discovery_n0().bind().await?;
+
+    let temp_path =
+        std::env::current_dir()?.join(PathBuf::from(format!(".temp_receiver_{}", ticket.hash().to_hex())));
+    dbg!(temp_path.clone());
+
+    if !temp_path.exists() {
+        create_dir_all(temp_path.clone())?;
+    }
+
+    let store = FsStore::load(temp_path.clone()).await?;
+    let hash = ticket.hash_and_format();
+    let local = store.remote().local(hash).await?;
+
+    if !local.is_complete() {
+        let addr = ticket.node_addr();
+        dbg!(addr.clone());
+        let con = endpoint.connect(addr.clone(), iroh_blobs::ALPN).await?;
+        dbg!(con.clone());
+        let g = store.remote().execute_get(con, local.missing()).await?;
+        println!(
+            "Completado en: {}/{}b",
+            g.elapsed.as_secs(),
+            g.total_bytes_read()
+        );
+    }
+    let collection = Collection::load(hash.hash, store.as_ref()).await?;
+    let first_name = collection.iter().next();
+
+    for (name, hash) in collection.iter() {
+    let current_path = std::env::current_dir()?;
+            let rel_path = 
+         if let Some(name_path) = path.clone() {
+            if !name_path.exists() {create_dir_all(name_path.clone())?}
+            println!("exporting to {:?}", name_path);
+            name_path.canonicalize()?
+        }
+        else{
+        println!("exporting to {:?}", current_path);
+        current_path
+        };
+        let file_path = rel_path.join(PathBuf::from(name));
+        dbg!(file_path.clone());
+        let mut stream_export = store
+            .export_with_opts(ExportOptions {
+                hash: *hash,
+                mode: ExportMode::Copy,
+                target: file_path,
+            })
+            .await?;
+        dbg!(stream_export);
+    }
+    remove_dir_all(temp_path).await?;
+    Ok(())
+}
--- src/p2p/send.rs
+++ src/p2p/send.rs
@@ -1,2 +1,101 @@
 
+use anyhow::{Context, Ok, Result};
+use clap::Parser;
+use iroh::{protocol::Router, Endpoint, SecretKey, Watcher};
+use iroh_blobs::{
+    api::{
+        blobs::{self, AddPathOptions, ExportMode, ExportOptions, ImportMode},
+        remote::GetProgressItem,
+        tags::TagInfo, Store,
+    }, format::collection::Collection, get::{self, Stats}, store::{fs::FsStore, mem::MemStore}, ticket::{self, BlobTicket}, BlobFormat, BlobsProtocol, Hash
+};
+use n0_future::{BufferedStreamExt, IterExt, StreamExt};
+use std::{
+    ffi::OsString,
+    fs::{self, create_dir_all},
+    path::{self, Path, PathBuf},
+};
+use tokio::{
+    fs::{create_dir, remove_dir_all},
+    sync::mpsc,
+};
+use walkdir::WalkDir;
+
+use crate::cli::commands::*;
+
+async fn send_path(path: PathBuf, blobs: BlobsProtocol,store: FsStore, router: Router) -> Result<()> {
+    let archivos = WalkDir::new(path.canonicalize()?.clone())
+        .into_iter()
+        .filter_map(|f| {
+            let f = f.ok()?;
+            dbg!(f.clone());
+
+            if !f.file_type().is_file() {
+                return None;
+            }
+            let name = f.clone().into_path().strip_prefix(path.canonicalize().unwrap().parent()?).unwrap().as_os_str().to_owned();
+            let ph = f.clone().into_path();
+
+            println!("{:?} {:?}", name, ph);
+
+            Some((name, ph))
+        })
+        .collect::<Vec<(OsString, PathBuf)>>();
+
+    dbg!(archivos.clone());
+
+    let res = n0_future::stream::iter(archivos)
+        .map(|(f, p)| {
+            let storage = blobs.store();
+            let blob = storage.add_path_with_opts(AddPathOptions {
+                path: p,
+                format: BlobFormat::Raw,
+                mode: ImportMode::TryReference,
+            });
+            async move { return (f.into_string().unwrap(), blob.await.unwrap().hash) }
+        })
+        .buffered_unordered(num_cpus::get())
+        .collect::<Vec<(String, Hash)>>()
+        .await;
+
+    let coleccion = Collection::from_iter(res);
+    dbg!();
+    let tag = coleccion.clone().store(&store).await?;
+    let addr = router.endpoint().node_addr().initialized().await;
+    let ticket = BlobTicket::new(addr, *tag.hash(), BlobFormat::HashSeq);
+    println!("{:?}", coleccion);
+    println!(" TICKET: {ticket}");
+
+    Ok(())
+}
+
+pub async fn send(path: Option<PathBuf>, database: Option<PathBuf>) -> Result<()> {
+    let mut rng = rand::rngs::OsRng;
+    let key =  SecretKey::generate(&mut rng);
+    let endpoint = Endpoint::builder().secret_key(key).discovery_n0().bind().await?;
+
+    let database_path: PathBuf = match database {
+        Some(ref a) => a.canonicalize()?,
+        None => std::env::current_dir()?.join(PathBuf::from(format!(
+            ".temp_sender_{:?}",
+            if let Some(p) = path.clone() {p.file_name().unwrap().to_os_string()}else{OsString::from("default")}
+        ))),
+    };
+
+    let store = FsStore::load(database_path.clone()).await?;
+
+    let blobs: BlobsProtocol = BlobsProtocol::new(&store, endpoint.clone(), None);
+
+    let router: Router = Router::builder(endpoint.clone())
+        .accept(iroh_blobs::ALPN, blobs.clone())
+        .spawn();
+
+    if path.is_some() {send_path(path.unwrap(),blobs,store,router.clone()).await?;}
+
+    tokio::signal::ctrl_c().await;
+
+    if database.is_none() {remove_dir_all(database_path).await?;}
+    router.shutdown().await?;
+    Ok(())
+}
 
